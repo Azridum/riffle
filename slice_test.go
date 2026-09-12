83a4426d10f93abf82a862f31441eb845869543a
Azridum <attila.banga29a@gmail.com>
2026-09-12 09:03:13 +0200

    test: rename

--- slice_test.go
+++ slice_test.go
@@ -692,7 +692,7 @@ func TestFind(t *testing.T) {
 	}
 }
 
-func TestAdd(t *testing.T) {
+func TestSum(t *testing.T) {
 	result, _ := riffle.Of(1, 0, 2, 3).Reduce(riffle.Sum)
 	test.Eq(t, result, 6)
 
