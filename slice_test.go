cd346681f144713e783b10b6e2e8c54fad771062
Azridum <attila.banga29a@gmail.com>
2026-09-12 08:57:59 +0200

    test: sum with fold

--- slice_test.go
+++ slice_test.go
@@ -695,4 +695,7 @@ func TestFind(t *testing.T) {
 func TestAdd(t *testing.T) {
 	result, _ := riffle.Of(1, 0, 2, 3).Reduce(riffle.Sum)
 	test.Eq(t, result, 6)
+
+	result = riffle.Of(1, 0, 2, 3).Fold(0.0, riffle.Sum)
+	test.Eq(t, result, 6)
 }
