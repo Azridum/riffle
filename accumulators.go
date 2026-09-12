944e4b94259c7d8fa488140203f7802df1336a50
Azridum <attila.banga29a@gmail.com>
2026-09-12 10:16:19 +0200

    chore: moved constraint

--- accumulators.go
+++ accumulators.go
@@ -4,13 +4,6 @@ import (
 	"cmp"
 )
 
-type number interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
-		~float32 | ~float64 |
-		~complex64 | ~complex128
-}
-
 // Sum returns the sum of a and b.
 //
 //	sum, ok := riffle.Of(1, 0, 2, 3).Reduce(riffle.Sum)
